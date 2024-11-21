3ce6d6c4a561726b9c41d33ae2c08071d5e3b32e
developerayo <shodipovi@gmail.com>
2024-11-21 16:01:05 +0100

    faster

--- lib/hooks/use-typewriter-effect.ts
+++ lib/hooks/use-typewriter-effect.ts
@@ -2,19 +2,22 @@ import { useEffect, useState } from "react";
 
 export const useTypewriter = (
   string: string = "",
-  speed = 50,
+  speed = 5,
   onComplete?: () => void
 ) => {
   const [displayString, setDisplayString] = useState("");
 
   useEffect(() => {
     let i = 0;
+    const charactersPerTick = 4;
+
     const typingInterval = setInterval(() => {
       if (i < string.length) {
-        setDisplayString(string.substr(0, i + 1));
-        i++;
+        setDisplayString(string.substr(0, i + charactersPerTick));
+        i += charactersPerTick;
       } else {
         clearInterval(typingInterval);
+        setDisplayString(string);
         if (onComplete) onComplete();
       }
     }, speed);
